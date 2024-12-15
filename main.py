82fb5fab2f08b063fc3d6733267cc391cc41a9a8
BoazAK <kodjoboaz@gmail.com>
2024-12-15 18:24:43 +0000

    Add of endpoint to get request header contents.

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
+# Root Path
 @app.get("/")
 async def home():
     return {
         "status" : "OK",
         "message" : "Server running"
     }
+
+# Get Headers
+@app.get("/get_headers", status_code = 200)
+async def get_headers(
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None),
+    host: str = Header(None)
+):
+
+    request_headers = {}
+
+    request_headers["Accept"] = accept
+    request_headers["Content-Type"] = content_type
+    request_headers["User-Agent"] = user_agent
+    request_headers["Host"] = host
+
+    return request_headers
