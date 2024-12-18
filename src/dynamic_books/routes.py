99f38174ae85c9c87bcc031774c27c16cc5e0c38
BoazAK <kodjoboaz@gmail.com>
2024-12-18 17:59:17 +0000

    Add of services file to dynamic_books to seperate the functions and use it ass class for services.

--- src/dynamic_books/routes.py
+++ src/dynamic_books/routes.py
@@ -7,6 +7,9 @@ from datetime import datetime, timezone
 from src.dynamic_books.schemas import Book, BookResponse, BookUpdateModel
 from src.config import db
 
+from src.dynamic_books.services import BookServices
+
+book_services = BookServices
 dynamic_book_router = APIRouter(
     tags = ["CRUD on Book with DataBase"]
 )
@@ -17,7 +20,7 @@ async def get_all_books(limit : int = 10, order_by : str = "created_at") :
 
     try :
 
-        books = await db["books"].find().sort(order_by, -1).to_list(limit)
+        books = await book_services.get_all_books(limit, order_by)
         
         return books
     
@@ -34,18 +37,9 @@ async def get_all_books(limit : int = 10, order_by : str = "created_at") :
 @dynamic_book_router.post("",  status_code = status.HTTP_201_CREATED, response_description = "Create a Book", response_model = BookResponse)
 async def create_a_book(book_data : Book) ->  dict :
     try :
-        new_book = jsonable_encoder(book_data)
+        new_book = await book_services.create_a_book(book_data)
 
-        # Add additional informations
-        new_book["created_at"] = str(datetime.now(timezone.utc))
-        new_book["publisher_user_id"] = "John Doe"
-        new_book["status"] = new_book["delete_status"] = False
-
-        new_book = await db["books"].insert_one(new_book)
-
-        created_book = await db["books"].find_one({"_id" : new_book.inserted_id})
-
-        return created_book
+        return new_book
 
     except Exception as e :
 
@@ -62,55 +56,9 @@ async def publish_a_book(id : str) :
         
     try :
 
-        book = await db["books"].find_one({"_id" : id, "delete_status" : False})
-        
-        if book is not None :
+        book = await book_services.publish_a_book(id)
 
-            try :
-
-                # Get current time
-                timestamp = {"published_at" : datetime.today()}
-                book_status = {"status" : True}
-
-                # Change data in JSON
-                json_timestamp = jsonable_encoder(timestamp)
-                json_book_status = jsonable_encoder(book_status)
-
-                # Merging JSON objects
-                book = {**book, **json_timestamp, **json_book_status}
-
-                update_result = await db["books"].update_one({"_id" : id}, {"$set" : book})
-
-                if update_result.modified_count == 1 :
-
-                    if (updated_book := await db["books"].find_one({"_id" : id })) is not None :
-
-                        return updated_book
-
-                if (existing_book := await db["books"].find_one({"_id" : id})) is not None :
-
-                    return existing_book
-                
-                raise HTTPException(
-                    status_code = status.HTTP_404_NOT_FOUND,
-                    detal = "The Book with this ID not found"
-                )
-            
-            except Exception as e :
-
-                print(f"Error occurred: {e}")
-                
-                raise HTTPException(
-                    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail = f"Internal server error: {str(e)}"
-                )
-            
-        else :
-
-            raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = "The Book with this ID not found"
-            )
+        return book
     
     except Exception as e :
 
@@ -126,7 +74,8 @@ async def publish_a_book(id : str) :
 async def get_all_published_books(limit : int = 10, order_by : str = "created_at") :
 
     try :
-        published_books = await db["books"].find({"status" : True, "delete_status" : False}).sort(order_by, -1).to_list(limit)
+
+        published_books = await book_services.get_all_published_books(limit, order_by)
         
         return published_books
     
@@ -145,64 +94,9 @@ async def unpublish_a_book(id : str) :
         
     try :
 
-        book = await db["books"].find_one({"_id" : id, "delete_status" : False})
-        
-        if book is not None :
+        book = await book_services.unpublish_a_book(id)
 
-            if book["status"] == False :
-
-                raise HTTPException(
-                    status_code = status.HTTP_304_NOT_MODIFIED,
-                    detail = "The Book is already published"
-                )
-            
-            elif book["status"] == True :
-
-                try :
-
-                    # Get current time
-                    timestamp = {"unpublished_at" : datetime.today()}
-                    book_status = {"status" : False}
-
-                    # Change data in JSON
-                    json_timestamp = jsonable_encoder(timestamp)
-                    json_book_status = jsonable_encoder(book_status)
-
-                    # Merging JSON objects
-                    book = {**book, **json_timestamp, **json_book_status}
-
-                    update_result = await db["books"].update_one({"_id" : id}, {"$set" : book})
-
-                    if update_result.modified_count == 1 :
-
-                        if (updated_book := await db["books"].find_one({"_id" : id })) is not None :
-
-                            return updated_book
-
-                    if (existing_book := await db["books"].find_one({"_id" : id})) is not None :
-
-                        return existing_book
-                    
-                    raise HTTPException(
-                        status_code = status.HTTP_404_NOT_FOUND,
-                        detal = "The Book with this ID not found"
-                    )
-            
-                except Exception as e :
-
-                    print(f"Error occurred: {e}")
-                    
-                    raise HTTPException(
-                        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
-                        detail = f"Internal server error: {str(e)}"
-                    )
-            
-        else :
-
-            raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = "The Book with this ID not found"
-            )
+        return book
     
     except Exception as e :
 
@@ -219,18 +113,9 @@ async def get_unpublished_books(limit : int = 10, order_by : str = "created_at")
         
     try :
 
-        unpub_book = await db["books"].find({"status": False, "delete_status" : False}).sort(order_by, -1).to_list(limit)
-        
-        if unpub_book is not None :
+        unpub_book = await book_services.get_unpublished_books(limit, order_by)
 
-            return unpub_book
-            
-        else :
-
-            raise HTTPException(
-                status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail = "Internal server error"
-            )
+        return unpub_book
     
     except Exception as e :
 
@@ -247,55 +132,9 @@ async def delete_a_book(id : str) :
         
     try :
         
-        book = await db["books"].find_one({"_id" : id, "delete_status" : True})
+        book = await book_services.delete_a_book(id)
 
-        if book is not None :
-
-            try :
-
-                # Get current time
-                timestamp = {"deleted_at" : datetime.today()}
-                book_status = {"delete_status" : True}
-
-                # Change data in JSON
-                json_timestamp = jsonable_encoder(timestamp)
-                json_book_status = jsonable_encoder(book_status)
-
-                # Merging JSON objects
-                book = {**book, **json_timestamp, **json_book_status}
-
-                deleted_result = await db["books"].update_one({"_id" : id}, {"$set" : book})
-
-                if deleted_result.modified_count == 1 :
-
-                    if (deleted_book := await db["books"].find_one({"_id" : id })) is not None :
-
-                        return deleted_book
-
-                if (existing_book := await db["books"].find_one({"_id" : id})) is not None :
-
-                    return existing_book
-                
-                raise HTTPException(
-                    status_code = status.HTTP_404_NOT_FOUND,
-                    detal = "The Book with this ID not found"
-                )
-        
-            except Exception as e :
-
-                print(f"Error occurred: {e}")
-                
-                raise HTTPException(
-                    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail = f"Internal server error: {str(e)}"
-                )
-
-        else :
-
-            raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = "The Book with this ID not found or The Book is already deleted"
-            )
+        return book
     
     except Exception as e :
 
@@ -312,21 +151,9 @@ async def get_deleted_books(limit : int = 10, order_by : str = "deleted_at") :
 
     try :
         
-        books = await db["books"].find({"delete_status" : True}).sort(order_by, -1).to_list(limit)
-        
-        if not books :
-            raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = "No deleted book found."
-            )
+        books = await book_services.get_deleted_books(limit, order_by)
 
-        result = []
-
-        for book in books :
-
-            result.append(BookResponse(**book))
-
-        return result
+        return books
 
     except Exception as e :
 
@@ -339,17 +166,11 @@ async def get_deleted_books(limit : int = 10, order_by : str = "deleted_at") :
         
 # Get one book
 @dynamic_book_router.get("/{id}", response_description = "Get a book", response_model = BookResponse)
-async def get_book(id : str) :
+async def get_a_book(id : str) :
 
     try :
-        book = await db["books"].find_one({"_id" : id, "status" : True, "delete_status" : False})
 
-        if book is None :
-
-            raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = "The Book with this ID not found"
-            )
+        book = await book_services.get_a_book(id)
 
         return book
     
@@ -368,57 +189,9 @@ async def update_book(id : str, book_data : Book) :
 
     try :
 
-        book = await db["books"].find_one({"_id" : id, "delete_status" : False})
+        book = await book_services.update_book(id, book_data)
 
-        if book is not None :
-
-            try :
-
-                book_data = {k : v for k, v in book_data.model_dump(exclude_unset=True).items() if v is not None}
-
-                if len(book_data) >=1 :
-
-                    # Get current time
-                    timestamp = {"updated_at" : datetime.today()}
-
-                    # Change data in JSON
-                    json_timestamp = jsonable_encoder(timestamp)
-
-                    # Merging JSON objects
-                    book_data = {**book_data, **json_timestamp}
-
-                    update_result = await db["books"].update_one({"_id" : id}, {"$set" : book_data})
-
-                    if update_result.modified_count == 1 :
-
-                        if (updated_book := await db["books"].find_one({"_id" : id })) is not None :
-
-                            return updated_book
-
-                    if (existing_book := await db["books"].find_one({"_id" : id})) is not None :
-
-                        return existing_book
-                    
-                    raise HTTPException(
-                        status_code = status.HTTP_404_NOT_FOUND,
-                        detal = "The Book with this ID not found"
-                    )
-            
-            except Exception as e :
-
-                print(f"Error occurred: {e}")
-                
-                raise HTTPException(
-                    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail = f"Internal server error: {str(e)}"
-                )
-
-        else :
-
-            raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = "The Book with this ID not found"
-            )
+        return book
     
     except Exception as e :
 
@@ -435,40 +208,9 @@ async def hard_delete_book(id : str) :
     
     try :
 
-        book = await db["books"].find_one({"_id" : id})
-
-        if book is not None :
-
-            try :
-
-                delete_result = await db["books"].delete_one({"_id" : id})
-                
-                if delete_result.deleted_count == 1 :
-                    
-                    return HTTPException(
-                        status_code = status.HTTP_204_NO_CONTENT,
-                    )
-                
-                raise HTTPException(
-                    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail = "Internal server error"
-                )
-
-            except Exception as e :
-
-                print(f"Error occurred: {e}")
+        book = await book_services.hard_delete_book(id)
         
-                raise HTTPException(
-                    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail = f"Internal server error: {str(e)}"
-                )
-            
-        else :
-
-            raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = "The Book with this ID not found"
-            )
+        return book
 
     except Exception as e :
 
